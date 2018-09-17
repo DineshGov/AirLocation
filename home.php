32b1f788bffb912825f5cceabdbe62ef567cab6c
DineshGov <gov.dinesh@gmail.com>
2018-09-17 14:12:05 +0200

    commit avant pull

--- home.php
+++ home.php
@@ -4,12 +4,26 @@
   //require('../database_auth.php');
 ?>
 
+<!DOCTYPE html>
+<html lang="fr">
+  <head>
+    <title>Se7en</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="jquery/jquery-3.3.1.js"></script>
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+    
+  </head>
+
   <div class="col-lg-12 col-md-12 col-sm-12">
 
-    <div class="col-lg-5 col-md-5 col-sm-5" style="border: 1px solid black">
+    <div class="col-lg-5 col-md-5 col-sm-5" style="border: 1px solid black; height: 400px;">
     </div>
 
-    <div class="col-lg-7 col-md-7 col-sm-7" style="border: 1px solid red">
+    <div class="col-lg-offset-1 col-lg-6 col-md-offset-1 col-md-6 col-sm-offset-1 col-sm-6" style="border: 1px solid red;  height: 400px;">
     </div>
 
   </div>
