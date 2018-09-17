33589acfc451a96cb3dfa509e4549e7a321efb2a
DineshGov <gov.dinesh@gmail.com>
2018-09-17 13:54:55 +0200

    ajout css/

---
+++ home.php
@@ -0,0 +1,18 @@
+<?php
+	$page_name="home.php";
+  //require ('header.php');
+  //require('../database_auth.php');
+?>
+
+  <div class="col-lg-12 col-md-12 col-sm-12">
+
+    <div class="col-lg-5 col-md-5 col-sm-5" style="border: 1px solid black">
+    </div>
+
+    <div class="col-lg-7 col-md-7 col-sm-7" style="border: 1px solid red">
+    </div>
+
+  </div>
+  
+</body>
+</html>
