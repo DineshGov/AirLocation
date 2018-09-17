dd7c3ff3fbc8a1a7c22bf9d7e65004b531716a42
Slimanes <slimane_kouba@yahoo.fr>
2018-09-17 14:11:09 +0200

    ajout de l'entete

---
+++ entete.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="utf-8">
+  <title>Research</title>
+    <link href="bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <script src="jquery/jquery-3.3.1.js"></script>  
+    <script src="bootstrap/js/bootstrap.min.js"></script>
+   
+</head>
+
+<body>
+
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">AirLocation</a>
+    </div>
+    <ul class="nav navbar-nav navbar-right">
+      <li><a href="#"><span class="glyphicon glyphicon-user"></span> Inscription</a></li>
+      <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Connexion</a></li>
+    </ul>
+  </div>
+</nav>
