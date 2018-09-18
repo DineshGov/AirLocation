1019e16dc26ab611ea96ea1445af17e526741ee7
DineshGov <gov.dinesh@gmail.com>
2018-09-18 10:30:13 +0200

    commit avant pull

--- mon_compte.php
+++ mon_compte.php
@@ -1,5 +1,13 @@
 <?php
 	$page_name="mon_compte.php";
-  //require ('header.php');
+	require ('entete.php');
   //require('../database_auth.php');
 ?>
+
+	<div class="col-lg-12 col-md-12 col-sm-12">
+		<h1 id="" >Test</h1>
+		<div class="col-lg-12 col-md-12 col-sm-12">
+			
+		</div>
+
+	</div>
