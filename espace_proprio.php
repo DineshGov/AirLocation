1cdf26c8c5ff3044279ac117df72de805a2f3b30
DineshGov <gov.dinesh@gmail.com>
2018-09-19 10:04:49 +0200

    commit avant pull

--- espace_proprio.php
+++ espace_proprio.php
@@ -7,7 +7,8 @@
 
 	<div class="col-lg-12 col-md-12 col-sm-12">
 
-        <div class="col-lg-offset-1 col-lg-11 col-md-offset-1 col-md-11 col-sm-offset-1 col-sm-11"> <h1>Ajouter un logement</h1>
+        <div class="col-lg-12 col-md-12 col-sm-12"> 
+        	<h1>Ajouter un logement</h1>
         </div>
 
     </div>
