d20542231d3dcf8f39b38b00e95c54b828f9bfcf
Slimanes <slimane_kouba@yahoo.fr>
2018-09-19 10:02:22 +0200

    new bdd insertions

--- airlocation.sql
+++ airlocation.sql
@@ -18,17 +18,27 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
---
--- Base de données :  `airlocation`
---
-
 -- --------------------------------------------------------
 
 --
--- Structure de la table `logements`
+-- Structure de la table `users`
 --
 
-DROP TABLE IF EXISTS `logements`;
+
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `idUser` smallint(6) NOT NULL AUTO_INCREMENT,
+  `login` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `password` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `nom` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `prenom` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
+  `is_owner` tinyint(1) NOT NULL,
+  PRIMARY KEY (`idUser`),
+  UNIQUE KEY `idUser` (`idUser`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
+
+
+
 CREATE TABLE IF NOT EXISTS `logements` (
   `idLogement` smallint(3) NOT NULL,
   `ville` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
@@ -46,29 +56,13 @@ CREATE TABLE IF NOT EXISTS `logements` (
   UNIQUE KEY `idLogement` (`idLogement`),
   KEY `idProprio` (`idProprio`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
-
---
--- Déchargement des données de la table `logements`
---
-
-INSERT INTO `logements` (`idLogement`, `ville`, `dateArr`, `dateDep`, `capacite`, `idProprio`, `longitude`, `latitude`, `typeLogement`, `prix`, `description`, `nomLogement`) VALUES
-(1, 'Paris', '2018-09-18', '2018-09-30', 4, 1, 2.26562, 48.8318, 'appartement', 61, 'L\'INTER-HOTEL PARISIANA à Paris jouit d\'un emplacement privilégié : situé dans un quartier populaire et vivant aux portes du 9ème arrondissement et à seulement 15 minutes de l\'Opéra Garnier. Il vous permet d\'accéder à tous les endroits de Paris ainsi qu\'aux nombreux points d\'intérêt de la capitale. ', 'Hôtel Parisiana'),
-(2, 'Paris', '2018-09-18', '2018-09-30', 4, 1, 2.32578, 48.8845, 'appartement', 89, 'L\'équipe de l\'Ibis Paris Montmartre 18ème vous accueille à l\'entrée d\'un lieu enchanteur. Prenez le temps de flâner dans les rues de la Butte Montmartre vers le Sacré Coeur. Ou pour une vie nocturne animée, découvrez Pigalle et son fameux Moulin Rouge.', 'Hôtel Montmartre'),
-(3,"Pantin","2018-05-27","2018-05-28",5,0,2.403295,48.891094,"hotel",50.5,"un hotel à pantin","Pantin"),
-(4,"Lille","2018-05-27","2018-05-28",5,0,3.069272,50.636186,"hotel",50.5,"un hotel à lille","Lille");
-
-
-
-
-
-
 -- --------------------------------------------------------
 
 --
 -- Structure de la table `reservations`
 --
 
-DROP TABLE IF EXISTS `reservations`;
+
 CREATE TABLE IF NOT EXISTS `reservations` (
   `idLogement` smallint(3) NOT NULL,
   `idUser` smallint(3) NOT NULL,
@@ -80,30 +74,21 @@ CREATE TABLE IF NOT EXISTS `reservations` (
   KEY `idUser` (`idUser`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
--- --------------------------------------------------------
-
---
--- Structure de la table `users`
---
-
-DROP TABLE IF EXISTS `users`;
-CREATE TABLE IF NOT EXISTS `users` (
-  `idUser` smallint(6) NOT NULL AUTO_INCREMENT,
-  `login` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `password` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `nom` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `prenom` varchar(100) CHARACTER SET utf8 COLLATE utf8_bin NOT NULL,
-  `is_owner` tinyint(1) NOT NULL,
-  PRIMARY KEY (`idUser`),
-  UNIQUE KEY `idUser` (`idUser`)
-) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8 ROW_FORMAT=COMPACT;
 
 --
 -- Déchargement des données de la table `users`
 --
 
-INSERT INTO `users` (`idUser`, `login`, `password`, `nom`, `prenom`, `is_owner`) VALUES
-(1, 'slim', 'azerty', 'kouba', 'slimane', 1);
+
+
+
+
+--
+-- Déchargement des données de la table `logements`
+--
+
+
+
 
 --
 -- Contraintes pour les tables déchargées
@@ -126,3 +111,14 @@ COMMIT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+INSERT INTO `users` (`idUser`, `login`, `password`, `nom`, `prenom`, `is_owner`) VALUES
+(1, 'slim', 'azerty', 'kouba', 'slimane', 1);
+
+
+INSERT INTO `logements` (`idLogement`, `ville`, `dateArr`, `dateDep`, `capacite`, `idProprio`, `longitude`, `latitude`, `typeLogement`, `prix`, `description`, `nomLogement`) VALUES
+(1, 'Paris', '2018-09-18', '2018-09-30', 4, 1, 2.26562, 48.8318, 'appartement', 61, 'L\'INTER-HOTEL PARISIANA à Paris jouit d\'un emplacement privilégié : situé dans un quartier populaire et vivant aux portes du 9ème arrondissement et à seulement 15 minutes de l\'Opéra Garnier. Il vous permet d\'accéder à tous les endroits de Paris ainsi qu\'aux nombreux points d\'intérêt de la capitale. ', 'Hôtel Parisiana'),
+(2, 'Paris', '2018-09-18', '2018-09-30', 4, 1, 2.32578, 48.8845, 'appartement', 89, 'L\'équipe de l\'Ibis Paris Montmartre 18ème vous accueille à l\'entrée d\'un lieu enchanteur. Prenez le temps de flâner dans les rues de la Butte Montmartre vers le Sacré Coeur. Ou pour une vie nocturne animée, découvrez Pigalle et son fameux Moulin Rouge.', 'Hôtel Montmartre'),
+(3,"Pantin","2018-05-27","2018-05-28",5,1,2.403295,48.891094,"hotel",50.5,"un hotel à pantin","Pantin"),
+(4,"Lille","2018-05-27","2018-05-28",5,1,3.069272,50.636186,"hotel",50.5,"un hotel à lille","Lille");
