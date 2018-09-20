1cf21a7be4f98927de289c1085a700917958cfeb
AssanDiomande <assanivory@gmail.com>
2018-09-20 16:27:14 +0200

    ...

--- airlocation.sql
+++ airlocation.sql
@@ -52,8 +52,7 @@ CREATE TABLE IF NOT EXISTS `reservations` (
   `idLogement` smallint(3) NOT NULL,
   `nbrVoyageur` int(11) NOT NULL,
   `dateArr` date NOT NULL,
-  `dateDep` date NOT NULL,
-  PRIMARY KEY (`idUser`,`idLogement`)
+  `dateDep` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_bin;
 
 -- --------------------------------------------------------
@@ -105,3 +104,6 @@ INSERT INTO `logements` (`idLogement`, `ville`, `dateArr`, `dateDep`, `capacite`
 (4,"Lille","2018-05-27","2018-05-28",5,1,3.069272,50.636186,"hotel",50.5,"un hotel à lille","Lille"),
 (5,"Gonesse","2018-05-27","2018-05-28",5,1,2.460192,48.979565,"hotel",50.5,"un hotel à gonesse","Gonesse"),
 (6,"Marseille","2018-05-27","2018-05-28",5,1,5.371591,43.312007,"hotel",50.5,"un hotel à marseille","Marseille");
+
+insert into reservations values(1,1,4,"2018-09-26","2018-09-28");
+insert into reservations values(1,1,4,"2018-09-22","2018-09-26")
